8d21175e076a9cc5a6e804550e1518be4570c9a5
nfreire <nfreire@gmail.com>
2022-02-02 09:54:34 +0000

    fix web resource

--- data-normalization-dates/src/main/java/europeana/rnd/dataprocessing/dates/DatesExtractorHandler.java
+++ data-normalization-dates/src/main/java/europeana/rnd/dataprocessing/dates/DatesExtractorHandler.java
@@ -141,7 +141,6 @@ public class DatesExtractorHandler {
 	
 	private void handleResult(String choUri, DateValue dateValue) {
 		try {
-			System.out.println(dateValue);
 			stats.add(choUri, dateValue);
 			Writer wrt=getWriterForMatch(dateValue.match.getMatchId());
 			wrt.write(escape(dateValue.match.getInput()));
--- data-normalization-dates/src/main/java/europeana/rnd/dataprocessing/dates/DatesHandler.java
+++ data-normalization-dates/src/main/java/europeana/rnd/dataprocessing/dates/DatesHandler.java
@@ -15,11 +15,13 @@ import inescid.dataaggregation.data.model.Edm;
 import inescid.dataaggregation.data.model.Ore;
 import inescid.dataaggregation.data.model.RdaGr2;
 import inescid.dataaggregation.data.model.Rdf;
+import inescid.dataaggregation.data.model.Skos;
 import inescid.util.RdfUtil;
 
 public class DatesHandler {
-		@SuppressWarnings("serial")
-		private static HashSet<Property> temporalProperties=new HashSet<Property>() {{
+	@SuppressWarnings("serial")
+	private static HashSet<Property> temporalProperties = new HashSet<Property>() {
+		{
 //			add(Dc.subject);
 //			add(Dc.coverage);
 			add(Dc.date);
@@ -41,95 +43,138 @@ public class DatesHandler {
 			add(RdaGr2.dateOfDeath);
 			add(RdaGr2.dateOfEstablishment);
 			add(RdaGr2.dateOfTermination);
-		}};
-
-		DatesJsonWriter jsonWriter;
-		
-		public DatesHandler(String outputFolder) {
-			super();
-			this.jsonWriter = new DatesJsonWriter(outputFolder);
 		}
-		public void handle(Model edm, int recCnt) throws IOException {
-			DatesInRecord res=getDatesInRecord(edm);
-			if(!res.isEmpty()) 
-				jsonWriter.write(res);
+	};
+
+	DatesJsonWriter jsonWriter;
+
+	public DatesHandler(String outputFolder) {
+		super();
+		this.jsonWriter = new DatesJsonWriter(outputFolder);
+	}
+
+	public void handle(Model edm, int recCnt) throws IOException {
+		DatesInRecord res = getDatesInRecord(edm);
+		if (!res.isEmpty())
+			jsonWriter.write(res);
 //			if (recCnt % 10000 == 0) {
 //			}
-		}
-		
-		public static DatesInRecord getDatesInRecord(Model edm) {
-			HashSet<String> processedResources=new HashSet<String>();
-			Resource choRes = RdfUtil.findFirstResourceWithProperties(edm, Rdf.type, Edm.ProvidedCHO, null, null);
-			String choUri = choRes.getURI();
-			DatesInRecord res=new DatesInRecord(choUri);
-			for(Resource proxy:edm.listResourcesWithProperty(Rdf.type, Ore.Proxy).toList()) {
-				processedResources.add(RdfUtil.getUriOrId(proxy));
-				Statement europeanaProxySt = proxy.getProperty(Edm.europeanaProxy);
-				boolean isEuropeanaProxy=europeanaProxySt!=null && europeanaProxySt.getObject().asLiteral().getBoolean();
-				for(Statement st: proxy.listProperties().toList()) {
-					if(st.getObject().isResource()) {
-						processInnerResource(res, st.getObject().asResource(), processedResources);
-					}else if(st.getObject().isLiteral()) {
-						if(isTemporalProp(st.getPredicate())) {
-							if(isEuropeanaProxy)
-								res.addToEuropeanaProxy(st.getPredicate(), st.getObject().asLiteral());
-							else
-								res.addToProviderProxy(st.getPredicate(), st.getObject().asLiteral());
+	}
+
+	public static DatesInRecord getDatesInRecord(Model edm) {
+//			HashSet<String> processedResources=new HashSet<String>();
+		Resource choRes = RdfUtil.findFirstResourceWithProperties(edm, Rdf.type, Edm.ProvidedCHO, null, null);
+		String choUri = choRes.getURI();
+		DatesInRecord res = new DatesInRecord(choUri);
+
+		for (Resource resType : new Resource[] { Ore.Proxy, Ore.Aggregation, Edm.WebResource, Edm.Agent, Edm.Place,
+				Edm.TimeSpan }) {
+			for (Resource proxy : edm.listResourcesWithProperty(Rdf.type, resType).toList()) {
+//				processedResources.add(RdfUtil.getUriOrId(proxy));
+				for (Statement st : proxy.listProperties().toList()) {
+					if (st.getObject().isLiteral()) {
+						if (isTemporalProp(st.getPredicate())) {
+							if (resType.equals(Edm.WebResource))
+								res.addToWebResources(st.getPredicate(), st.getObject().asLiteral());
+							else if (resType.equals(Edm.Agent))
+								res.addToAgents(st.getPredicate(), st.getObject().asLiteral());
+							else if (resType.equals(Edm.TimeSpan))
+								res.addToTimeSpans(st.getPredicate(), st.getObject().asLiteral());
+							else if (resType.equals(Ore.Proxy)) {
+								Statement europeanaProxySt = proxy.getProperty(Edm.europeanaProxy);
+								boolean isEuropeanaProxy = europeanaProxySt != null
+										&& europeanaProxySt.getObject().asLiteral().getBoolean();
+								if (isEuropeanaProxy)
+									res.addToEuropeanaProxy(st.getPredicate(), st.getObject().asLiteral());
+								else
+									res.addToProviderProxy(st.getPredicate(), st.getObject().asLiteral());
+							} else if (resType.equals(Ore.Aggregation)) {
+								System.out.println("WARNING: temporal prop in ore:Aggregation - " + st);
+							} else if (resType.equals(Edm.ProvidedCHO)) {
+								System.out.println("WARNING: temporal prop in edm:ProvidedCHO - " + st);
+							}
 						}
 					}
 				}
 			}
-			Resource aggRes = RdfUtil.findFirstResourceWithProperties(edm, Rdf.type, Ore.Aggregation, null, null);
-			processedResources.add(RdfUtil.getUriOrId(aggRes));
-			for(Statement st: aggRes.listProperties().toList()) {
-				if(st.getObject().isResource()) {
-					processInnerResource(res, st.getObject().asResource(), processedResources);
-				}else if(st.getObject().isLiteral()) {
-					//TODO: should properties in aggregation be processed?
-					if(isTemporalProp(st.getPredicate())) 
-						System.out.println("WARNING: temporal prop in ore:Aggregation - "+st);
-				}
-			}
-			return res;
-		}
-		
-		private static boolean isTemporalProp(Property predicate) {
-			return temporalProperties.contains(predicate);
-		}
 
-		public void finalize() throws IOException {
-			jsonWriter.close();
 		}
-
-		private static void processInnerResource(DatesInRecord res, Resource resource, HashSet<String> processedResources) {
-			if(processedResources.contains(RdfUtil.getUriOrId(resource)))
-				return;
-			processedResources.add(RdfUtil.getUriOrId(resource));
-			
-			List<Resource> types = RdfUtil.getTypes(resource);
-			if(types==null || types.isEmpty()) return;
-			Resource type=null;
-			if(types.contains(Edm.WebResource))
-				type=Edm.WebResource;
-			else if(types.contains(Edm.Agent))
-				type=Edm.Agent;
-			else if(types.contains(Edm.TimeSpan))
-				type=Edm.TimeSpan;
-			else
-				return;
-			for(Statement st: resource.listProperties().toList()) {
-				if(st.getObject().isResource()) {
-					processInnerResource(res, st.getObject().asResource(), processedResources);
-				}else if(st.getObject().isLiteral()) {
-					if(isTemporalProp(st.getPredicate())) {
-						if(type.equals(Edm.WebResource))
-							res.addToWebResources(st.getPredicate(), st.getObject().asLiteral());
-						else if(type.equals(Edm.Agent))
-							res.addToAgents(st.getPredicate(), st.getObject().asLiteral());
-						else if(type.equals(Edm.TimeSpan))
-							res.addToTimeSpans(st.getPredicate(), st.getObject().asLiteral());
-					}
-				}
-			}
-		}
+		return res;
 	}
+
+//		public static DatesInRecord getDatesInRecord(Model edm) {
+//			HashSet<String> processedResources=new HashSet<String>();
+//			Resource choRes = RdfUtil.findFirstResourceWithProperties(edm, Rdf.type, Edm.ProvidedCHO, null, null);
+//			String choUri = choRes.getURI();
+//			DatesInRecord res=new DatesInRecord(choUri);
+//			for(Resource proxy:edm.listResourcesWithProperty(Rdf.type, Ore.Proxy).toList()) {
+//				processedResources.add(RdfUtil.getUriOrId(proxy));
+//				Statement europeanaProxySt = proxy.getProperty(Edm.europeanaProxy);
+//				boolean isEuropeanaProxy=europeanaProxySt!=null && europeanaProxySt.getObject().asLiteral().getBoolean();
+//				for(Statement st: proxy.listProperties().toList()) {
+//					if(st.getObject().isResource()) {
+//						processInnerResource(res, st.getObject().asResource(), processedResources);
+//					}else if(st.getObject().isLiteral()) {
+//						if(isTemporalProp(st.getPredicate())) {
+//							if(isEuropeanaProxy)
+//								res.addToEuropeanaProxy(st.getPredicate(), st.getObject().asLiteral());
+//							else
+//								res.addToProviderProxy(st.getPredicate(), st.getObject().asLiteral());
+//						}
+//					}
+//				}
+//			}
+//			Resource aggRes = RdfUtil.findFirstResourceWithProperties(edm, Rdf.type, Ore.Aggregation, null, null);
+//			processedResources.add(RdfUtil.getUriOrId(aggRes));
+//			for(Statement st: aggRes.listProperties().toList()) {
+//				if(st.getObject().isResource()) {
+//					processInnerResource(res, st.getObject().asResource(), processedResources);
+//				}else if(st.getObject().isLiteral()) {
+//					//TODO: should properties in aggregation be processed?
+//					if(isTemporalProp(st.getPredicate())) 
+//						System.out.println("WARNING: temporal prop in ore:Aggregation - "+st);
+//				}
+//			}
+//			return res;
+//		}
+
+	private static boolean isTemporalProp(Property predicate) {
+		return temporalProperties.contains(predicate);
+	}
+
+	public void finalize() throws IOException {
+		jsonWriter.close();
+	}
+//
+//		private static void processInnerResource(DatesInRecord res, Resource resource, HashSet<String> processedResources) {
+//			if(processedResources.contains(RdfUtil.getUriOrId(resource)))
+//				return;
+//			processedResources.add(RdfUtil.getUriOrId(resource));
+//			
+//			List<Resource> types = RdfUtil.getTypes(resource);
+//			if(types==null || types.isEmpty()) return;
+//			Resource type=null;
+//			if(types.contains(Edm.WebResource))
+//				type=Edm.WebResource;
+//			else if(types.contains(Edm.Agent))
+//				type=Edm.Agent;
+//			else if(types.contains(Edm.TimeSpan))
+//				type=Edm.TimeSpan;
+//			else
+//				return;
+//			for(Statement st: resource.listProperties().toList()) {
+//				if(st.getObject().isResource()) {
+//					processInnerResource(res, st.getObject().asResource(), processedResources);
+//				}else if(st.getObject().isLiteral()) {
+//					if(isTemporalProp(st.getPredicate())) {
+//						if(type.equals(Edm.WebResource))
+//							res.addToWebResources(st.getPredicate(), st.getObject().asLiteral());
+//						else if(type.equals(Edm.Agent))
+//							res.addToAgents(st.getPredicate(), st.getObject().asLiteral());
+//						else if(type.equals(Edm.TimeSpan))
+//							res.addToTimeSpans(st.getPredicate(), st.getObject().asLiteral());
+//					}
+//				}
+//			}
+//		}
+}
